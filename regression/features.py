73d49fdbbdd8cd7dc4985b18b297491c125a9e52
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-10-15 16:27:04 -0500

    adding features to Features

--- regression/features.py
+++ regression/features.py
@@ -5,9 +5,25 @@
 # able to use those two attributes as features to estimate total_charges. I 
 # need to do this within an average of $5.00 per customer.
 
+# prepare enviornment
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore")
 
+import env
+import wrangle
+import split_scale
 
+# For Problem 1:
+from sklearn.feature_selection import SelectKBest, f_regression 
+# For Problem 3:
+import statsmodels.api as sm
 
 ###############################################################################
 # 
@@ -15,11 +31,22 @@
 # and k as input (X_train and y_train should not be scaled!) and returns a 
 # list of the top k features.
 
+def select_kbest_freg_unscaled(X_train, y_train, k=2):
+    f_selector = SelectKBest(f_regression, k=k)
+    f_selector.fit(X_train, y_train)
+    f_support = f_selector.get_support()
+    f_feature = X_train.loc[:,f_support].columns.tolist()
+    return f_feature, f_selector
+
 
 ###############################################################################
 # 
 # 2. Write a function, `select_kbest_freg_scaled()` that takes X_train, y_train 
 # (scaled) and k as input and returns a list of the top k features.
+
+def select_kbest_freg_scaled(X_train_scaled, y_train_scaled, k=2):
+    f_feature_scaled, f_selector_scaled = select_kbest_freg_unscaled(X_train=X_train_scaled, y_train=y_train_scaled, k=k)
+    return f_feature_scaled, f_selector_scaled
 
 
 ###############################################################################
@@ -27,6 +54,31 @@
 # 3. Write a function, `ols_backward_elimination()` that takes X_train and 
 # y_train (scaled) as input and returns selected features based on the ols 
 # backwards elimination method.
+
+def ols_backward_elimination(X_train_scaled, y_train_scaled):
+    cols = list(X_train_scaled.columns)
+    cols_removed = []
+    removed_pvals = []
+    pmax = 1
+    while (len(cols)>0):
+        p= []
+        X_1 = X_train[cols]
+        X_1 = sm.add_constant(X_1)
+        model = sm.OLS(y_train_scaled,X_1).fit()
+        p = pd.Series(model.pvalues.values[1:],index = cols)
+        pmax = max(p)
+        feature_with_p_max = p.idxmax()
+        if(pmax>0.05):
+            cols_removed.append(feature_with_p_max)
+            removed_pvals.append(pmax)
+            cols.remove(feature_with_p_max)
+        else:
+            break
+            
+    selected_features_BE = cols
+    removed_features_BE = cols_removed
+    removed_pvals_BE = removed_pvals
+    return selected_features_BE, removed_features_BE, removed_pvals_BE
 
 
 ###############################################################################
