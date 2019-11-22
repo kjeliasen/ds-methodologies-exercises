29ebca5d96c85706dcf86fa80134f22726ec2363
Kevin Eliasen <kevin.eliasen@gmail.com>
2019-11-22 11:29:40 -0600

    continuous problems

---
+++ anomaly_detection/continuous_probabilistic_methods.py
@@ -0,0 +1,20 @@
+###############################################################################
+### pthon imports                                                           ###
+###############################################################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+###############################################################################
+### local imports                                                           ###
+###############################################################################
+
+#from env import host, user, password
+from debug import local_settings, timeifdebug, timeargsifdebug, frame_splain
